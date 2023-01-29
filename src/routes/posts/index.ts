f0a9b87a077d90f79605f762c5bd0d578155977f
Igor Badyukov <badyukov1806302003@gmail.com>
2023-01-29 21:35:41 +0300

    feat: add endpoint to post

--- src/routes/posts/index.ts
+++ src/routes/posts/index.ts
@@ -1,12 +1,16 @@
 import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
 import { idParamSchema } from '../../utils/reusedSchemas';
 import { createPostBodySchema, changePostBodySchema } from './schema';
-// import type { PostEntity } from '../../utils/DB/entities/DBPosts';
+import type { PostEntity } from '../../utils/DB/entities/DBPosts';
+import {HttpError} from "@fastify/sensible/lib/httpError";
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
 ): Promise<void> => {
-  fastify.get('/', async function (request, reply): Promise</* PostEntity[]*/ void> {});
+  fastify.get('/', async function (request, reply): Promise<PostEntity[]> {
+      const allPosts = await fastify.db.posts.findMany();
+      return allPosts;
+  });
 
   fastify.get(
     '/:id',
@@ -15,7 +19,14 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise</* PostEntity */ void> {}
+    async function (request, reply): Promise<PostEntity | HttpError> {
+        const postID = request.params.id;
+        const post = await fastify.db.posts.findOne({key: "id", equals: postID});
+        if (!post) {
+            return fastify.httpErrors.notFound();
+        }
+        return post;
+    }
   );
 
   fastify.post(
@@ -25,7 +36,14 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         body: createPostBodySchema,
       },
     },
-    async function (request, reply): Promise</* PostEntity */ void> {}
+    async function (request, reply): Promise<PostEntity | HttpError> {
+        const userID = request.body.userId;
+        const user = await fastify.db.users.findOne({key: "id", equals: userID});
+        if (!user) {
+            return fastify.httpErrors.badRequest();
+        }
+        return await fastify.db.posts.create(request.body);
+    }
   );
 
   fastify.delete(
@@ -35,7 +53,15 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise</* PostEntity */ void> {}
+    async function (request, reply): Promise<PostEntity | HttpError> {
+        const postID = request.params.id
+        const post = await fastify.db.posts.findOne({key: "id", equals: postID});
+
+        if (!post) {
+            return fastify.httpErrors.badRequest();
+        }
+        return await fastify.db.posts.delete(request.params.id);
+    }
   );
 
   fastify.patch(
@@ -46,7 +72,14 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise</* PostEntity */ void> {}
+    async function (request, reply): Promise<PostEntity | HttpError> {
+        const postID = request.params.id;
+        const post = await fastify.db.posts.findOne({key: "id", equals: postID});
+        if (!post) {
+            return fastify.httpErrors.badRequest();
+        }
+        return await fastify.db.posts.change(request.params.id, request.body);
+    }
   );
 };
 
