1fbb6473d9ad6ef045416eb2476be36ac068d1fc
Igor Badyukov <badyukov1806302003@gmail.com>
2023-01-29 22:02:22 +0300

    feat: add endpoint to memberType

--- src/routes/member-types/index.ts
+++ src/routes/member-types/index.ts
@@ -1,14 +1,15 @@
 import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
 import { idParamSchema } from '../../utils/reusedSchemas';
 import { changeMemberTypeBodySchema } from './schema';
-// import type { MemberTypeEntity } from '../../utils/DB/entities/DBMemberTypes';
+import type { MemberTypeEntity } from '../../utils/DB/entities/DBMemberTypes';
+import {HttpError} from "@fastify/sensible/lib/httpError";
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
 ): Promise<void> => {
-  fastify.get('/', async function (request, reply): Promise<
-    /* MemberTypeEntity[] */ void
-  > {});
+  fastify.get('/', async function (request, reply): Promise<MemberTypeEntity[]> {
+      return await fastify.db.memberTypes.findMany();
+  });
 
   fastify.get(
     '/:id',
@@ -17,7 +18,14 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise</* MemberTypeEntity */ void> {}
+    async function (request, reply): Promise<MemberTypeEntity | HttpError> {
+        const memberTypeID = request.params.id
+        const memberType = await fastify.db.memberTypes.findOne({key: "id", equals: memberTypeID});
+        if (!memberType) {
+            return fastify.httpErrors.notFound();
+        }
+        return memberType;
+    }
   );
 
   fastify.patch(
@@ -28,7 +36,17 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise</* MemberTypeEntity */ void> {}
+    async function (request, reply): Promise<MemberTypeEntity | HttpError> {
+        const memberType = await fastify.db.memberTypes.findOne({key: "id", equals: request.params.id});
+        if (!memberType) {
+            return fastify.httpErrors.badRequest();
+        }
+        const changeMemberType = await fastify.db.memberTypes.change(request.params.id, request.body);
+        if (!changeMemberType) {
+            return fastify.httpErrors.notFound();
+        }
+        return changeMemberType;
+    }
   );
 };
 
